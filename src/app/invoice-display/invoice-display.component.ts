cafa1865a4ef92d86600088e3fbea0c53a2d3c8b
tjdiya <91diya@gmail.com>
2019-08-21 01:51:36 +0530

    dummy

--- src/app/invoice-display/invoice-display.component.ts
+++ src/app/invoice-display/invoice-display.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
 export class InvoiceDisplayComponent {
     constructor() {}
     @Input('invItem') invoiceItem: {type: string, itemName: string, price: number};
-
+ 
 }
